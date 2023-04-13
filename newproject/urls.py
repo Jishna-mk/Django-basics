fd8870a85c3b7c7090ee1fcd6123c6689c6000d7
Jishna mk <jishnamk9@gmail.com>
2023-04-13 16:45:01 +0530

    user authentication creation

--- newproject/urls.py
+++ newproject/urls.py
@@ -6,7 +6,7 @@ from. import views
 
 urlpatterns=[
     path('',views.home, name='home'),
-    path('room/',views.room,name='home'),
+    path('room/',views.room,name='room'),
     path('navbar/',views.navbar,name='navbar'),
     path('main/',views.main,name='main'),
     path('login/',views.login,name='login'),
--- newproject/views.py
+++ newproject/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render 
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -15,11 +16,32 @@ def navbar(request):
     return render(request,'navbar.html')
 def main(request):
     return render(request,'main.html')
-def login(request):
-    return render(request,'loginview.html')
+
 def signup(request):
+    if request.method=="POST":
+        uname=request.POST.get('Username')
+        email=request.POST.get('email')
+        pass1=request.POST.get('password1')
+        pass2=request.POST.get('password2')
+        
+        if pass1!=pass2:
+            return HttpResponse("pass word mismatch")
+        
+        else:
+         my_user=User.objects.create_user(uname,email,pass1)
+         my_user.save()
+         return HttpResponse("new user created")
+    
+    # return redirect('login')
     return render(request,'registerview.html')
 
+def login(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('password')
+        
+        
+    return render(request,'loginview.html')
 
 
 
